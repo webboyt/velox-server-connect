962e493a3b1da51b52b706b6adaba80467f1d5f9
SimpsonGames <56755656+SimpsonGames@users.noreply.github.com>
2025-09-03 16:32:02 +0100

    Refactored main class captialisation

--- src/main/java/velox/server_connect/VeloxServerConnect.java
+++ src/main/java/velox/server_connect/VeloxServerConnect.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Veloxserverconnect implements ModInitializer {
+public class VeloxServerConnect implements ModInitializer {
 	public static final String MOD_ID = "velox-server-connect";
 
 	// This logger is used to write text to the console and the log file.
